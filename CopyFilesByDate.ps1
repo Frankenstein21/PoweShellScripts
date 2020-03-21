6e0aaa40980affd43977f009fee2626b2c452250
Frankier Flores <frankierflores@gmail.com>
2020-03-21 16:42:56 -0600

    Script para copiar archivos

---
+++ CopyFilesByDate.ps1
@@ -0,0 +1,17 @@
+﻿# Este Script copia los archivos creados o modificados a partir de una fecha indicada.
+Clear-Host
+
+$Origen = Read-Host "Escibe la dirección de origen"
+while (-NOT (Test-Path $Origen)) {
+    Write-Host "El directorio no existe. Escribe un dirección válida"
+    $Origen = Read-Host "Escibe la direcció de origen"
+}
+
+$Destino = Read-Host "Escribe la dirección de destino"
+If (-NOT (Test-Path $Destino)) { 
+    New-Item -ItemType Directory $Destino | Out-Null 
+} 
+ 
+$Fecha = Read-Host "Especifíca la antiguedad máxima (yyyyMMdd), ejemplo: 20200320"
+
+Robocopy $Origen $Destino /MAXAGE:$Fecha /s
