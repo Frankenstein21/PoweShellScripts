9df23d1b3ac76312143198d2e7a2bd607ab6cb22
Frankier Flores <frankierflores@gmail.com>
2020-03-21 17:25:03 -0600

    Creación de un usuario

---
+++ CreateUser.ps1
@@ -0,0 +1,5 @@
+﻿# Creación de un usuario
+$usuario = Read-Host "Introduce el nombre de usuario"
+$pass = Read-Host "Introduce la contraseña" -AsSecureString
+New-LocalUser $usuario -Password $pass
+Add-LocalGroupMember usuarios -Member $usuario
