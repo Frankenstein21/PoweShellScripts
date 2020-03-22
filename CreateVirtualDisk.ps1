fc0aee8f77950000699dc3178219c736d14d7aa4
Frankier Flores <frankierflores@gmail.com>
2020-03-21 18:07:16 -0600

    Creación de un disco virtual

---
+++ CreateVirtualDisk.ps1
@@ -0,0 +1,8 @@
+﻿# creación automática de un disco virtual
+$disco = Read-Host "Nombre del disco (path): "
+[double]$tamano = Read-Host "Tamaño del disco en Bytes: "
+New-VHD -Path $disco -SizeBytes $tamano -Dynamic
+Mount-VHD -Path $disco -Passthru|
+Initialize-Disk -PassThru|
+New-Partition -AssignDriveLetter -UseMaximumSize|
+Format-Volume -FileSystem NTFS -Confirm:$false
