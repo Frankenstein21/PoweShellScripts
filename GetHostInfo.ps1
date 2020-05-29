51c5d4181f1c3538453190a51f5e3396f363536c
Frankier Flores Zamora <frankierflores@PROMUJER.local>
2020-05-29 15:56:33 -0600

    Scripts para obtener información del equipo y sistema operativo

---
+++ GetHostInfo.ps1
@@ -0,0 +1,43 @@
+﻿# Descripción: Script para obtener información básica del equipo.
+#              Se utiliza el estándar CIM, clase Win32_ComputerSystem.
+# Fecha: Mayo 2020
+
+Clear-Host
+
+# Obtener nombre del Host
+$HostName = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
+
+# Obtener el nombre del dominio
+$Domain = (Get-CimInstance -ClassName Win32_ComputerSystem).Domain
+
+# Obtener el nombre de usuario
+$UserName = (Get-CimInstance -ClassName Win32_ComputerSystem).UserName
+
+# Obtener tipo de equipo (Desktop o Laptop)
+$Type = (Get-CimInstance -ClassName Win32_ComputerSystem).ChassisSKUNumber
+
+# Obtener el fabricante del equipo
+$Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+
+# Obtener el modelo del equipo
+$Model = (Get-CimInstance -ClassName Win32_ComputerSystem).Model
+
+# Obtener la cantidad de memoria total
+$Memory = (Get-CimInstance -ClassName Win32_ComputerSystem).TotalPhysicalMemory
+
+# Obtener la cantidad de procesadores
+$NumberProcessors = (Get-CimInstance -ClassName Win32_ComputerSystem).NumberOfProcessors
+
+# Obtener la cantidad de procesadores
+$NumberCores = (Get-CimInstance -ClassName Win32_ComputerSystem).NumberOfLogicalProcessors
+
+# Write-Host "$HostName,$Domain,$UserName,$Type,$Manufacturer,$Model,$Memory,$NumberProcessors,$NumberCores"
+
+Write-Host "Nombre de host: $HostName"
+Write-Host "Nombre de dominio: $Domain"
+Write-Host "Nombre de usuario: $UserName"
+Write-Host "Tipo de equipo: $Type"
+Write-Host "Fabricante: $Manufacturer"
+Write-Host "Modelo: $Model"
+Write-Host "Memoria instalada: $Memory"
+Write-Host "Procesadores: $NumberProcessors procesador y $NumberCores núcleos"
---
+++ GetOsInfo.ps1
@@ -0,0 +1,36 @@
+﻿# Descripción: Script para obtener información básica del sistema operativo.
+#              Se utiliza el estándar CIM, clase Win32_OperatingSystem.
+# Fecha: Mayo 2020
+
+Clear-Host
+
+# Obtener nombre del sistema operativo
+$OsName = (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
+
+# Obtener la versión del sistema operativo
+$OsVersion = (Get-CimInstance -ClassName Win32_OperatingSystem).Version
+
+# Obtener el número de compilación del sistema
+$BuildNumber = (Get-CimInstance -ClassName Win32_OperatingSystem).BuildNumber
+
+# Obtener el tipo de arquitectura del sistema
+$Architecture = (Get-CimInstance -ClassName Win32_OperatingSystem).OSArchitecture
+
+# Obtener la fecha de instalación del sistema
+$InstallDate = (Get-CimInstance -ClassName Win32_OperatingSystem).InstallDate
+# $InstallDate = Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -Property InstallDate
+
+# Obtener fecha y hora del último arranque del sistema
+$LastBootUp = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
+# $LastBootUp = Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -Property LastBootUpTime
+
+# La opción de imprimir los valores en una líena se puede usar para manipular los datos desde otra aplicación, por ejemplo, GNet.
+# Write-Host "$OsName,$OsVersion,$BuildNumber,$InstallDate,$FreeMemory,$VirtualMemory,$FreeVirtual,$LastBootUp"
+
+# Imprimir valores en pantalla
+Write-Host "Sistema Operativo: $OsName"
+Write-Host "Versión del sistema: $OsVersion"
+Write-Host "Número de compilación: $BuildNumber"
+Write-Host "Arquitectura del sistema: $Architecture"
+Write-Host "Fecha de instalación: $InstallDate"
+Write-Host "Último arranque: $LastBootUp"
