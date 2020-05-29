61af30e8b65b86d840eaab65dc50a124de7bf792
Frankier Flores Zamora <frankierflores@PROMUJER.local>
2020-05-29 11:29:50 -0600

    Script para obtener información del procesador

---
+++ GetCpuStatus.ps1
@@ -0,0 +1,13 @@
+﻿# Descripción: El Script sirve para monitorizar el estado de la CPU.
+# obtiene información del procesador, número de núcleos y el procentaje de uso
+# Fecha: Mayo 2020  
+Clear-Host
+
+$Name = (Get-CimInstance -ClassName Win32_Processor).Name
+$NumberCores = (Get-CimInstance -ClassName Win32_Processor).NumberOfCores
+$Load = (Get-CimInstance -ClassName Win32_Processor).LoadPercentage
+
+# Write-Host "$Name,$NumberCores,$Load"
+Write-Host "Procesador: $Name"
+Write-Host "Núcleos: $NumberCores"
+Write-Host "En uso: $Load%"
