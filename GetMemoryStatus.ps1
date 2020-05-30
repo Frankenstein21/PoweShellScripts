2faba908d9398d30a64ba59c5dbac69a3e605683
Frankier Flores Zamora <frankierflores@PROMUJER.local>
2020-05-29 21:22:06 -0600

    Iniciando Script para ver información de la memoria

---
+++ GetMemoryStatus.ps1
@@ -0,0 +1,21 @@
+﻿
+Clear-Host
+
+$Total = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -Property TotalPhysicalMemory | ForEach-Object {$_.TotalPhysicalMemory / 1GB}
+$TotalMemory = (Get-CimInstance -ClassName Win32_ComputerSystem).TotalPhysicalMemory
+$FreeMemory = (Get-CimInstance -ClassName Win32_OperatingSystem).FreePhysicalMemory
+$VirtualMemory = (Get-CimInstance -ClassName Win32_OperatingSystem).TotalVirtualMemorySize
+$FreeVirtual = (Get-CimInstance -ClassName Win32_OperatingSystem).FreeVirtualMemory
+$Memory = (Get-CimInstance -ClassName Win32_PhysicalMemory).Capacity
+$Capacity = Get-CimInstance -ClassName Win32_PhysicalMemory | Select-Object -Property Capacity | ForEach-Object {$_.capacity / 1GB}
+
+
+
+Write-Host "Memoria total: $TotalMemory"
+Write-Host "$Total"
+Write-Host "Memoria disponible: $FreeMemory"
+Write-Host "Memoria virtual: $VirtualMemory"
+Write-Host "Memoria virtual disponible: $FreeVirtual"
+
+Write-Host "$Memory"
+Write-Host "$Capacity"
