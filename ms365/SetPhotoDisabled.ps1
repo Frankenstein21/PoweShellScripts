859fbb55c90e69ab93cdfbe176459e0f491ccda7
Frankier Flores <frankierflores@gmail.com>
2023-10-18 22:00:42 -0600

    Desactivar el cambio de foto en Microsoft 365 y actualizar foto de perfil a todo los usuarios

---
+++ ms365/SetPhotoDisabled.ps1
@@ -0,0 +1,9 @@
+﻿# Conectar a Exhange Online
+# Connect-ExchangeOnline -UserPrincipalName admin@test.onmicrosoft.com
+
+# Obtener y listar las políticas de buzón de correo de OWA
+# Get-OwaMailboxPolicy | Select-Object Identity, SetPhotoEnabled
+
+# Modificar la política de buzón de correo de OWA específica
+$policyName="OwaMailboxPolicy-Default"
+Set-OwaMailboxPolicy -Identity "$policyName" -SetPhotoEnabled $false
---
+++ ms365/UploadProfilePhoto.ps1
@@ -0,0 +1,14 @@
+﻿# Conectar a Exhange Online
+# Connect-ExchangeOnline -UserPrincipalName admin@test.onmicrosoft.com
+
+# Ruta de la imagen que se asignará como foto de perfil
+$imagenPath = "C:\temp\2.jpg"
+
+# Obtener todos los buzones de correo y establecer la foto de perfil
+Get-EXOMailbox -ResultSize Unlimited | ForEach-Object {
+    Set-UserPhoto -Identity "$_" -PictureData ([System.IO.File]::ReadAllBytes("$imagenPath")) -Confirm:$false
+    $_.DisplayName
+}
+
+# Desconectar la sesión de PowerShell
+# Disconnect-ExchangeOnline -Confirm:$false
